e4f12e2edb96c7c32c3c7546349b99fa7227b0da
tmico <thierrymicoud@gmail.com>
2016-03-15 22:41:04 +0000

    improvements to code

--- console.s
+++ console.s
@@ -4,7 +4,6 @@
 	
 .text
 	.align 2
-	.global _print_line
 	.global _print_terminal_buffer 
 	.global _printf
 	
@@ -17,7 +16,7 @@ _write_terminal_buffer:
 
 	/*_write_terminal_buffer will take a string (ascii) passed via a pointer
 	 *  of a mem address in r0, the number of chars to be printed in r1
-
+	*/
 _print_terminal_buffer:			@ Funtional code to start. TODO colour support
 /* _print_terminal_buffer prints the content of TermBuffer within the range of
  *	TermScreen. TermScreen is what is displayed on screen but is
@@ -26,108 +25,83 @@ _print_terminal_buffer:			@ Funtional code to start. TODO colour support
  *	_print_terminal_buffer sends line by line the content of TermBuffer updating
  *	TermCur (current line), TermStart and TermEnd
 */
-	stmfd sp!, {r4 - r8, lr}
-	ldr r12, =TermStart			@ ldr buffer* var
-	ldr r4, [r12]
-	ldr r12, =TermEnd
-	ldr r5, [r12]
-	ldr r12, =TermCur
-	ldr r6, [r12]
-	ldr r7, =TermBuffer			@ Not content just address
-	ldr r12, =CursorLoc
-	ldr r8, [r12, #4]
-
-	cmp r6, $45				@ tst if on bottom line
-	bleq _scroll_page
-
-_PB1:
-
-	add r1, r4, r6				@ add line number to TermStart
-	and r1, r1, $0x7f			@ mask to keep looping TermBuffer
-	mov r0, r1, lsl $6
-	add r0, r0, r1, lsl $4			@ mul by 80
-	add r1, r7, r0				@ r1 points to loc in TermBuffer
-	bl _print_line
-
-	ldr r12, =CursorLoc
-	cmp r0, $'\n'				@ new line then loop
-	add r6, $1				@ next line
-	add r8, r8, $0x16			@ next line
-	str r8, [r12, #4]
-	bne _exit0
-	beq _PB1
-
-	cmp r6, $45
-	bmi _PB1
-
-_scroll_page:
-	push {lr}
-	ldr r12, =TermStart
-	addeq r4, r4, $1			@ if > scroll down buffer 
-	andeq r4, r4, $0x7f			@ 0 - 127
-	streq r4, [r12]
-
-	ldr r12, =TermEnd
-	addeq r2, r2, $1 
-	andeq r2, r2, $0x7f
-	streq r2, [r12]
-
-	ldr r12, =TermCur
-	mov r6, $0x0				@ reset curent line
-	str r6, [r12]
+	scratch .req r12
+	tc .req r4				@ current line
+	tb .req r5				@ termial buffer 
+	x .req r6 				@ x [0-79]
+	y .req r7				@ y [1-45]
 	
-	ldr r12, =CursorLoc
-	mov r8, $0x00				@ mov CursorLoc back to top
-	str r8, [r12, #4]
-
-	bl _init_dma0				@ clear thte screen
-	pop {pc}
+	stmfd sp!, {r4 - r11, lr}
+	ldr scratch, =TermCur
+	ldr tc, [scratch]
+	ldr scratch, =CursorLoc
+	ldrd x, y, [scratch]
+	ldr tb, =TermBuffer			@ Not content just address
 	
-_exit0:
-	ldr r12, =TermStart			@ save buffer* var
-	str r4, [r12]
-	ldr r12, =TermEnd
-	str r5, [r12]
-	ldr r12, =TermCur
-	str r6, [r12]
-	ldr r12, =TermBuffer
-	str r7, [r12]
+	add r1, x, tc, lsl $6
+	add r1, r1, tc, lsl $4			@ mem loc = tc * 80 + x
+	ldrb r0, [tb, r1]!			@ note write back
+_TB:	
+	cmp r0, $0x20				@ tst for non printable char
+	bmi _non_print
+	mov r1, x, lsl $3			@ * 8 = pixel loc across	
+	mov r2, y, lsl $4			@ * 16 = pixel loc down
+	bl _print_to_screen
+	add x, x, $1
+	cmp x, $80
+	beq _new_line
+	ldrb r0, [tb, $1]!
+	b _TB
+_new_line:
+	ldr tb, =TermBuffer			@ reset tb as its a cyclic buffer
+	mov x, $0
+	add y, y, $1
+	add tc, tc, $1
+	and tc, tc, $0x7f			@ current line is looping
+	mov r1, tc, lsl $6
+	add r1, r1, tc, lsl $4			@ mem loc = tc * 80 
+	ldrb r0, [tb, r1]!			@ note write back
+	b _TB
+	
+_non_print:
+	cmp r0, $'\n'
+	beq _new_line
+	cmp r0, $0
+	ldr scratch, =TermCur
+	ldr r1, =CursorLoc
+	str tc, [scratch]
+	strd x, y, [r1]
+	ldmfd sp!, {r4-r11, pc}
 
-	ldmfd sp!, {r4-r8, pc}			@ anything other then exit
+.unreq tc
+.unreq tb
+.unreq x 
+.unreq y
+.unreq scratch
 
-
-_print_line:
-/* _print_line: for decoding the ascii string and selecting
- *	correct font. By default string will be	sent to stdout (screen).
- *	_print_terminal_buffer passes the address of the start of the string in r1.
- *	_print_line keeps on printing till a '\n' or 0x00 byte is met.
+_print_to_screen:
+/* _print_to_screen: for decoding the ascii char and selecting
+ *	correct font. By default char will be sent to stdout (screen).
 */
-	sadr .req r11				@ string addr
-	gadr .req r10				@ glyph addr
-	x .req r6				@ x coordinate
-	y .req r7				@ y coordinate
-	c16 .req r9				@ row counter 16 (down)
-	char .req r5				@ char to be printed
-	bmap .req r5				@ bitmap
-	xy .req r4				@ cursor loc
-	tmp .req r8				@ scratch
+	gadr .req r8				@ glyph addr
+	x .req r9				@ x coordinate
+	y .req r10				@ y coordinate
+	c16 .req r11				@ row counter 16 (down)
+	char .req r4				@ char to be printed
+	bmap .req r4				@ bitmap
 
-	/* r4,r12 free */
-	stmfd sp!, {r4-r11, lr}			@ This section loads font to use
-
+/* in future additional routine to choose font placed here */
+	push {r4, lr}
 _u_vga16:
 	ldr gadr, =Uvga16
-	ldr xy, =CursorLoc
-	mov sadr, r1
-	add gadr, gadr, $0x30			@ add offset to bitmaps
-	ldrb char, [sadr], #1
-	ldrd x, y, [xy]
-_F1:	
-	subs char, char, $0x20
-	bmi _special_char
-	add tmp, gadr, char, lsl #4
-	ldrb bmap, [tmp], #1
+	sub char, r0, $0x20
 	mov c16, $0x10				@ 16 rows
+	add gadr, gadr, $0x30			@ add offset to bitmaps
+
+	add gadr, gadr, char, lsl #4
+	ldrb bmap, [gadr], #1
+	mov x, r1
+	mov y, r2
 _F2:
 	movs bmap, bmap, lsl #24		@ endianes issue plus allows
 	beq _F4					@  testing using N flag
@@ -174,32 +148,16 @@ _F2:
 _F4:
 	add y, y, $0x01				@ set coordinates for next row
 	subs c16, c16, $1
-	ldrneb bmap, [tmp], #1
+	ldrneb bmap, [gadr], #1
 	bne _F2
 
-	add x, x, $0x08				@ set next char coordinates
-	sub y, y, $0x10
-
-	ldrb char, [sadr], #1
-	b _F1
-
-
-_special_char:
-/* TO Finish*/
-	strd x, y, [xy]
-	cmn char, $20				@ -20 = null char(see _F1 )
-	cmn char, $10				@ -10 = '\n'
-	moveq r0, $10
-	ldmfd sp!, {r4-r11, pc}			@ exit
+	pop {r4, pc}
 
 .unreq char
 .unreq x
 .unreq y
-.unreq xy
-.unreq tmp
-.unreq bmap
-.unreq sadr
 .unreq gadr
+.unreq bmap
 .unreq c16
 	
 _bin_asciihex:
@@ -355,7 +313,7 @@ TermBuffer:
 	.endr					@  a roll over count 
 
 	.global ScreenBuffer
-ScreenBuffer:					@ tmp to use by dma_0 to clear
+ScreenBuffer:					@ gadr to use by dma_0 to clear
 	.rept 0x1400				@  screen
 	.byte 0x00
 	.endr
