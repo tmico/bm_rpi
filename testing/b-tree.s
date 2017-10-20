e687ae5f457e465019c5e9abaa986422ef7ba6b9
hiro <hiro@nagate.pyrama.net>
2017-10-20 18:35:45 +0100

    'Pen to paper' a rough implemantation of a b-tree to use as a scheduling queue

--- testing/b-tree.s
+++ testing/b-tree.s
@@ -1,4 +1,4 @@
-/* Rough initial implimentation of a red-black tree
+/* Rough initial implimentation of a sort of balanced binary tree
  * (super rough !!!!)
  * to use as a task schedular based on very limited understanding
  * by far the hardest will be to add/remove and rebalance tree
@@ -8,14 +8,79 @@
 	.align 2
 _pick_tree:
 	/* pick_tree picks the key from the tree. Head always points to lowest
-	 * value fruit (key)
+	 * value fruit (key) and is a leaf
 	 */
-	stmfd sp!, {r4 - r11}
+	stmfd sp!, {r4 - r7, lr}
 	ldr r12, =hb_cur_list		@ get the hb_list to pick from
 	ldr r3, [r12]
-	ldrd r0, r1, [r3]		@ get head, root
-	ldrd r4, r5, [r0, $8] 		@ r4 = parent, r5 = pid 
+	ldrd r6, r7, [r3]		@ get head, root
+	ldrd r4, r5, [r6, $8] 		@ r4 = parent, r5 = pid 
+	cmp r6, r7
+	beq _sw
+	mov r0, r4
+	bl _reset_head
+_q:
+	mov r0, r5
+	ldmfd sp!, {r4 -r7, pc}
+
+_sw:
+	bl _swap_btree
+	b _q
+
+_reset_head:
+	/* input r0 = node from which to find new head in b-tree
+	 * if there is or are children of higher 'fruit[s]' then a singal rotate left
+	 * is performed on that branch
+	 */
+	stmfd sp!, {lr}
+	ldr r1, =hb_cur_list		@ get the hb_list to pick from
+	ldr r12, [r0, $4]		@ get value of > child
+	ldr r2, [r1]
+	mov r3, $0
+	cmp r12, $0			@ test if any > children
+	str r3, [r0]			@ null < branch
+	str r0, [r2, $4]		@ save new head
+	blne _rotate_left		@ if r1(child)!=0 ; need to rotate left
+	ldmfd sp!, {pc}
 	
+_rotate_left:
+	/* r0 = pivot point*/
+	.equ gtrchild, 4
+	.equ parent, 8
+	stmfd sp!, {r4 - r5}
+	ldr r4, [r0, #gtrchild] 
+	ldr r5, [r0, #parent]
+	mov r3, $0
+	str r3, [r0, #gtrchild]
+	str r4, [r0, #parent]
+	str r5, [r4, #parent]
+	str r4, [r5]
+	/* loop to find leaf to graft new leaf to */
+	ldr r4, [r4]
+_find_lsr_leaf:
+	cmp r4, $0
+	ldrne r4, [r4]
+	bne _find_lsr_leaf 
+	str r0, [r4]
+	str r4, [r0]
+	ldmfd sp!, {r4 - r5}
+	bx lr
+
+_swap_btree:
+	/* swap current and next list round */
+	ldr r12, =hb_cur_list
+	ldrd r0, r1, [r12]
+	str r0, [r12, $4]
+	str r1, [r12]
+	bx lr
+
+_graft_new_fruit:
+	/* insert new fruit (key) into b-tree. If graft is > than root then
+	 * rotate left is performed to make <greater than child> of root the
+	 * new root. Old_root becomes <lesser than child> of new_root
+	 * The <lesser than branch> of the new_root is 'dettached' and
+	 * and moved to become the new <greater than branch> of the old_root
+	 */
 
 	.data
 	.align 2
@@ -26,13 +91,14 @@ P_entry:
 	.word 0		@ PV   --priority val
 	.word 0		@ PSTATE {1 - 7?}
 		.rept 17	
-	.word 0			@ PREG  --saved registers {r0 - r16, cprs}
+	.word 0			@ PREG  --saved registers {r0 - r15, cprs}
 		.endr
 	.word 0
 		.rept 32
 	.asciz "\0"		@ PNAME --process name
 		.endr
 
+	.align 3	@ align 3 to allow ldrd/strd
 hb_cur_list:		@ current process list to 'pick' from
 	.word	hb_list0
 hb_next_list:		@ next list to 'graft' in
@@ -57,6 +123,8 @@ hb_list1:
 		.rept 20 @ Max number of process (can be changed later)
 	.word 0		@ * < node
 	.word 0		@ * > node
+	.word 0		@ parent addr
 	.word 0		@ pid addr
 	.word 0		@ KEY == PV
+	.word 0		@ 
 		.endr
