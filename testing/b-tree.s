e9fe2c4ce7e1e37975455988c16c32d476810922
tmx_izana <tmx@izana.pyrama.net>
2018-05-30 00:01:03 +0000

    queues and trees tested and appear to work

--- testing/b-tree.s
+++ testing/b-tree.s
@@ -16,68 +16,49 @@
 _pick_tree:
 	/* pick_tree picks the key from the tree. Head always points to lowest
 	 * value fruit (key) and is a leaf
+	 * Out: r0 = pid (&P_entry)
 	 */
-	stmfd sp!, {r4 - r7, lr}
+	stmfd sp!, {r4 - r7}
 	ldr r12, =hb_cur_list		@ get the hb_list to pick from
 	ldr r3, [r12]
-	ldrd r6, r7, [r3]		@ get head, root
-	ldrd r4, r5, [r6, $8] 		@ r4 = parent, r5 = pid 
-	cmp r6, r7			@ head == root?
-	beq _sw
-	mov r0, r4
-	bl _reset_head
-_q:
-	mov r0, r5
-	ldmfd sp!, {r4 -r7, pc}
-
-_sw:
 	mov r0, $0
-	mov r1, r0
-	strd r0, r1, [r3]		@ zero out head and tail
-	bl _swap_btree
-	b _q
+	mov r1, $0
+	ldrd r6, r7, [r3]		@ get root, head
+	ldrd r4, r5, [r7, $8]
 
 _reset_head:
-	/* input r0 = node from which to find new head in b-tree
-	 * if there is or are children of higher 'fruit[s]' then a single rotate left
-	 * is performed on that branch
+	/* reset head by finding new lowest hb_entry.
+	 * r3 = &hb_list
+	 * r4 = parent of hb_entry being popped (old head)
+	 * r7 = hb_entry being popped (old head)
 	 */
-	stmfd sp!, {lr}
-	ldr r1, =hb_cur_list		@ get the hb_list to pick from
-	ldr r12, [r0, $4]		@ get value of > child
-	ldr r2, [r1]
-	mov r3, $0
-	cmp r12, $0			@ test if any > children
-	str r3, [r0]			@ null < branch
-	str r0, [r2, $4]		@ save new head
-	blne _rotate_head		@ if r1(child)!=0 ; need to rotate left
-	ldmfd sp!, {pc}
-	
-_rotate_head:
-	/* r0 = pivot point
-	 * As head should always be the lowest key then only a left rotate is
-	 * needed, and branches rearanged to relocate head to lowest leaf
-	 */
-	.equ gtrchild, 4
-	.equ parent, 8
-	stmfd sp!, {r4 - r5}
-	ldr r4, [r0, #gtrchild]
-	ldr r5, [r0, #parent]
-	mov r3, $0
-	str r3, [r0, #gtrchild]
-	str r4, [r0, #parent]
-	str r5, [r4, #parent]
-	str r4, [r5]
-	/* loop to find leaf to graft new leaf to */
-	ldr r4, [r4]
-_find_lsr_leaf:
-	cmp r4, $0
-	ldrne r4, [r4]
-	bne _find_lsr_leaf 
-	str r0, [r4]
-	str r4, [r0]
-	ldmfd sp!, {r4 - r5}
+	ldr r2, [r6, $4]		@ r2 = root > child to check empty tree?
+	ldr r0, [r7, $4]		@ r0 = grtr child
+	cmp r2, $0			@ if root > == 0 and...
+	cmpeq r6, r7			@ ...head == tail then tree now empty
+	beq _sw
+	cmp r0, $0			@ if no grtr child ...
+	str r0, [r4]			@   (if nz then link child to new...
+	strne r4, [r0, $8]		@   ...parent)
+	moveq r0, r4			@ ...get new head from parent instead
+	ldr r2, [r0]			@ r2 = < child
+_fh:
+	cmp r2, $0
+	movne r0, r2
+	ldrne r2, [r0]
+	bne _fh
+	str r0, [r3, $4]		@ reset HEAD
+_q:
+	mov r0, r5
+	ldmfd sp!, {r4 - r7}
 	bx lr
+	
+_sw:
+	strd r0, r1, [r3]		@ zero out head and tail
+	mov r7, lr
+	bl _swap_btree
+	mov lr, r7
+	b _q
 
 @------------------------------------------------
 _swap_btree:
@@ -95,32 +76,37 @@ _graft_new_fruit:
 	 * new root. Old_root becomes <lesser than child> of new_root
 	 * The <lesser than branch> of the new_root is 'dettached' and
 	 * and moved to become the new <greater than branch> of the old_root
-	 * Input: R0 = Pid addr
-	 *	  R1 = hb_list{x}
+	 * Input: R0 = &P_entry
 	 */
 	stmfd sp!, {r4 - r6, lr}
+	ldr r3, =hb_cur_list
+	ldr r1, [r3]
 	ldr r3, [r1]			@ get root
+	ldr r6, [r0, $4]		@ get P_entry PV
 	mov r4, r0
 	mov r5, r1
 	cmp r3, $0			@ fresh hb_list?
 	beq _init_root
-	ldr r6, [r0, $4]		@ get PV
 	ldr r1, [r3, $16]		@ get roots key
-	ldrd r2, r3, [r3]		@ get children
+	mov r0, r3			@ copy to preserve
+	ldrd r2, r3, [r0]		@ get children
 	mov r12, r1			@ copy roots key to cmp later
 fl:					@ find leaf
-	cmp r1, r6
-	mov r0, r2			@ keep copy as future parent
+	cmp r6, r1			@ cmp the PV's
 	movmi r3, r2
-	mov r0, r3			@ keep copy as future parent
 	cmp r3, $0			@ leaf?
+	movne r0, r3			@ keep copy as future parent
 	ldrne r1, [r0, $16]		@ get key
 	ldrned r2, r3, [r0]		@ get children
-	bne _fl
+	bne fl
 _graft:	
 	/* r0 parent, load r2 P_entry's (PID) hbslb addr, r4 PID's addr */
 	ldr r2, [r4, $12]		@ get P_entry's alloted hbslb slot
-	str r2, [r3]			@ str hbslb addr into branch
+	ldr r4, [r5, $4]		@ get HEAD add to cmp PV with poss later
+	cmp r6, r1			@ < or > branch?
+	strmi r2, [r0]			@ str hbslb addr into branch
+	strpl r2, [r0, $4]		@ str hbslb addr into branch
+	str r6, [r2, $16]		@ str pv in hbslb->key
 	str r0, [r2, $8]		@ set parent of P_entry slot
 g1:	
 	mov r3, $0
@@ -130,21 +116,32 @@ g1:
 _balance_tree:
 	/*if PID PV greater than root PV then rotate left*/
 	cmp r12, r6			@ r12 == root_key, r6 == graft_key
-	bpl _exit_graft
+	bpl _pre_exit
 
-	ldr r3, [r5]			@ ldr root from (r5 == hb_list{x})
-	ldr r1 [r3, $4]			@ ldr root > child
+	ldr r3, [r5]			@ r3 = root hb
+	ldr r1, [r3, $4]		@ r1 = roots > child
 	mov r2, $0
+	cmp r1, $0
+	beq _exit_graft
 	/*r1 will be new root, r3 lesser child to root.*/
 	ldr r0, [r1]			@ r0 == r1 < child, will become r3 > child
 	str r3, [r1]			@ r3 bcomes r1 < child
 	str r1, [r3, $8]		@ r1 becomes r3 parent
 	str r0, [r3, $4]		@ r0 becomes r3 > child
 	str r1, [r5]			@ r1 becomes new root
-	str r2, [r1, $12]		@ root's parent zero'd out 
+	str r2, [r1, $8]		@ root's parent zero'd out 
 
 _exit_graft:
-	ldmfd sp!, {r4 - r6, lr}
+	ldmfd sp!, {r4 - r6, pc}
+
+_pre_exit:
+	/* cmp P_entry.PV with HEAD.PV and reset HEAD if 
+	 * P_entry.PV < HEAD.PV
+	 */
+	ldr r3, [r4, $16]		@ get HEAD.PV
+	cmp r6, r3
+	strmi r2, [r5, $4]		@ if < reset head
+	b _exit_graft
 
 _init_root:
 	/* If hb_list is 'fresh' then first new p_entry grafted will
@@ -153,7 +150,12 @@ _init_root:
 	ldr r2, [r4, $12]		@ get hbslb alloted slot
 	str r2, [r5]			@ set root
 	str r2, [r5, $4]		@ set head (remember its a fresh list)
-	b gl				@ gl zero's children
+
+	str r3, [r2]			@ zero out children
+	str r3, [r2, $4]
+	str r3, [r2, $8]
+	str r6, [r2, $16]		@ str pv in hbslb->key
+	b _exit_graft			@ gl zero's children
 
 @------------------------------------------------
 _getpid:
@@ -194,7 +196,7 @@ hbslb:			@ hb_shared_linked_basket */
 	.word 0		@ * < child addr 
 	.word 0		@ * > child addr
 	.word 0		@ parent addr
-	.word 0		@ pid addr
+	.word 0		@ pid addr (&P_entry)
 	.word 0		@ KEY == PV
 	.word 0		@ 
 		.endr
--- testing/double-linked-queue.s
+++ testing/double-linked-queue.s
@@ -8,7 +8,7 @@
 	 * empty padding
 	 * head  fp	1st qent
 	 * tail  fp	TAIL+4	
-	 *	 bp 	last item
+	 *	 bp 	previous item
 	 *	 pseudo item
 	 *	 pseudo key (signed int with value 0x8ffffffff)
 	 *--------------------
@@ -24,6 +24,7 @@
 	.global _getfirst
 	.global _getlast
 	.global _insert
+	.global _newqueue
 
 	.equ HEAD,	8
 	.equ TAIL,	12
@@ -32,16 +33,16 @@
  *	In:	r0 = &queue, r1 = &qent
  * 	Return: r0 = address on success -1 on failure
  */
+ @-- tested ok 17-5-18
 _enqueue:
 	stmfd sp!, {lr}
 	bl _get_loc
-	ldr r3, [r0, $TAIL]		@ r3 = tail
 	add r12, r0, $TAIL		@ r12 = &Tail
 
 	/* Append to queue */
 	str r12, [r1]			@ new qent fp = &tail
-	ldr r2, [r3, $4]		@ ldr bp of TAIL
-	str r1, [r3, $4]		@ reset tail to new qent
+	ldr r2, [r12, $4]		@ ldr bp of TAIL
+	str r1, [r12, $4]		@ reset tail to new qent
 	str r2, [r1, $4]		@ bp of new_qent --> header/old_qent
 	str r1, [r2]			@ fp of old_qent --> new_qent
 
@@ -53,22 +54,27 @@ _enqueue:
 	bx lr
 
 /*
- * deueue: removes item from head of list/queue
+ * getfirst: alias for dequeue
+ * dequeue: removes item from head of list/queue
  *	In:	r0 = &queue
- *	Return: r0 &qent on success, -1 on failure
+ *	Return: r0 &qent on success, 0 on failure
  */
+ @-- tested ok 17-5-18
+	.type _getfirst	%function
+	.type _dequeue	%function
 _dequeue:
+_getfirst:
 	stmfd sp!, {lr}
 	bl _get_loc
 	ldr r2, [r0, $HEAD]		@ r2 = head
 
-	/* Is queue empty? If so, no qent to 'pop', return -1 */
+	/* Is queue empty? If so, no qent to 'pop', return 0 */
 	add r12, r0, $TAIL		@ r12 = &tail
-	subne r3, r12, $4		@ r3 = &head
 	cmp r2, r12			@ does head --> tail?
-	ldrne r0, [r2]			@ r0 = &qent to pop
-	moveq r0, $-1			@ will return -1 if empty
+	movne r0, r2			@ r0 = &qent to pop
+	moveq r0, $0			@ will return 0 if empty
 	ldrne r1, [r0]			@ r1 = next qent to be head of list
+	sub r2, r12, $4			@ r2 = &head
 	strne r2, [r1, $4]		@ bp of next qent points to HEAD
 	strne r1, [r12, $-4]		@ reset head to --> to next qent
 
@@ -79,34 +85,28 @@ _dequeue:
 	mcr p15, 0, r3, c7, c10, 5	@ DMB
 	bx lr
 
-/*
- * getfirst: removes the first item (head) on a list
- *	In:	r0 = &queue
- *	Return: r0 = &qent on success, -1 on failure
- */
- _getfirst:
- 	b _dequeue
-
 
 /*
  * getlast: removes the last item (tail) on a list
  *	In:	r0 = &queue
- * 	Return: r0 = &qent on success, -1 on failure
+ * 	Return: r0 = &qent on success, 0 on failure
  */
+ @-- tested ok 15-5-18
+	.type _getlast	%function
  _getlast:
 	stmfd sp!, {lr}
 	bl _get_loc
-	ldr r2, [r0, $(TAIL+4)]		@ r2 = bp of tail
+	ldr r3, [r0, $(TAIL+4)]		@ r2 = bp of tail
 
-	/* Is queue empty? If so, no qent to 'pop', return -1 */
+	/* Is queue empty? If so, no qent to 'pop', return 0 */
 	add r12, r0, $HEAD		@ r12 = &HEAD
-	add r3, r12, $8			@ r3 = &TAIL
-	cmp r2, r12			@ does head <-- tail?
-	ldrne r0, [r2]			@ r0 = &qent to pop
-	moveq r0, $-1			@ queue empty ? r0 = -1
+	cmp r3, r12			@ does head <-- tail?
+	movne r0, r3			@ r0 = &qent to pop
 	ldrne r1, [r0, $4]		@ r1 = next qent to be tail of list
-	strne r1, [r12, $4]		@ reset tail bp to --> to previous qent
-	strne r3, [r1]			@ fp of next qent --> TAIL
+	moveq r0, $0			@ queue empty ? r0 = 0
+	add r2, r12, $4			@ r2 = &TAIL's bp
+	strne r1, [r12, $8]		@ reset tail bp to --> to previous qent
+	strne r2, [r1]			@ fp of next qent --> TAIL
 
 	/* Exit */
 	ldmfd sp!, {lr}
@@ -121,19 +121,22 @@ _dequeue:
  *		r1 = &qent
  * 	Return:	r0 = 0 on success, -1 of failure
  */
+ @-- tested ok 15-5-18
+	.type _insert	%function
+_insert:
 	stmfd sp!, {r4 - r5, lr}
 	bl _get_loc
-	ldr r2, [r0, $HEAD]		@ r2 = fp of HEAD
-	ldr r4, [r1, $12]		@ r4 = key of qent to insert
+	add r2, r0, $HEAD		@ r2 = &(fp of HEAD)
 	ldr r3, [r2]			@ r3 = addr of 1st qent on list
+	ldr r4, [r1, $12]		@ r4 = key of qent to insert
 	
 	/* loop through list till find a higher key value  */
 _1:	ldr r5, [r3, $12]		@ r5 = key of qent loaded
 	mov r12, r2			@ preserve previous fp
 	mov r2, r3
 	cmp r4, r5			@ cmp the keys
-	ldrlt r3, [r2]			@ r3 = next qent
-	blt _1
+	ldrhi r3, [r2]			@ r3 = next qent
+	bhi _1
 	
 	str r1, [r12]			@ previous qent's fp
 	str r1, [r3, $4]		@ current qent's bp
@@ -152,3 +155,27 @@ _1:	ldr r5, [r3, $12]		@ r5 = key of qent loaded
  *        In: r0 address of header for new queue/list
  *        Return: r0 = address of new queue on success, 0 on failure
  */
+ @-- tested ok 14-5-18
+	.type _newqueue	%function
+_newqueue:
+	mov r1, $1
+	str r1, [r0]			@ create the lock
+	add r1, r0, $12			@ r1 = &TAIL
+	str r1, [r0, $HEAD]
+	str r1, [r0, $TAIL]
+	add r1, r0, $8			@ r1 = &HEAD
+	str r1, [r0, $(TAIL+4)]
+	mov r1, $0xffffffff		@ pseudo key = largest unsigned value
+	str r1, [r0, $(TAIL+12)]
+	mov r1, $0			@ unlock new list
+	str r1, [r0]
+
+	/* exit */
+	bx lr
+
+@----- to delete -----
+_get_loc:
+	mov r12, $1
+	str r12, [r0]
+	bx lr
+@---------------------
